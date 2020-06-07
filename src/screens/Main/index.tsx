055774da657b2f2f90574e452195eca454211f24
intensifies <aintensifies@gmail.com>
2020-06-07 12:09:04 +0700

    fix: текст для главной

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Typography, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  link: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    height: '64px',
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    cursor: 'pointer',
+    '&:hover': {
+      background: theme.palette.primary.dark,
+    },
+  },
+  container: {
+    width: 960,
+    margin: 'auto',
+  },
+}))
 
 export const Main = (): JSX.Element => {
+  const classes = useStyles()
+
   return (
     <>
-      <div>Shmain</div>
+      <div className={classes.container}>
+        <Typography variant="h2">lang-beta.now.sh</Typography>
+        <Typography variant="h6">
+          это онлайн-плеер для просмотра фильмов на английском языке. Ключевая
+          особенность плеера — Перевод субтитров по клику мыши!
+        </Typography>
+        <br />
+        <Typography variant="h6">
+          Пример. Кликните на любое слово в субтитрах, чтобы увидеть перевод.
+        </Typography>
+      </div>
     </>
   )
 }
