59d0d209274cc966f065ee0ead81abb662b9ee10
intensifies <aintensifies@gmail.com>
2020-06-12 21:14:25 +0700

    fix: dont deploy bundle analizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,7 @@
 const merge = require('webpack-merge')
 const commonConfig = require('./webpack.common')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
 
 module.exports = merge(commonConfig, {
   mode: 'production',
-  plugins: [
-    new BundleAnalyzerPlugin({ openAnalyzer: false, analyzerMode: 'static' }),
-  ],
+  plugins: [],
 })
